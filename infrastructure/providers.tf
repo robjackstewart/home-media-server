adff205970b5d0864b06d97d9f13cee65386a575
Rob Jack Stewart <robjackstewart@gmail.com>
2023-10-24 20:53:39 +0100

    move to azure state store

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -13,4 +13,11 @@ terraform {
       version = "~> 2.0"
     }
   }
+
+  backend "azurerm" {
+      resource_group_name  = "tfstate"
+      storage_account_name = "robstewarttfstate"
+      container_name       = "tfstate"
+      key                  = "home-media-server.terraform.tfstate"
+  }
 }
