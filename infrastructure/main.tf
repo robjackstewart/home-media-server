ef63956b69b5f1c0cab08c9d9a57dfee9124ac28
Rob Jack Stewart <robjackstewart@gmail.com>
2025-01-30 21:53:00 +0000

    upgrade cloudflare

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -35,12 +35,12 @@ resource "random_id" "argo_secret" {
 }
 
 resource "cloudflare_zero_trust_tunnel_cloudflared" "tunnel" {
-  account_id = data.azurerm_key_vault_secret.cloudflare_account_id.value
-  name       = var.cloudflare_tunnel_name
-  secret     = random_id.argo_secret.b64_std
-  config_src = "local"
+  account_id    = data.azurerm_key_vault_secret.cloudflare_account_id.value
+  name          = var.cloudflare_tunnel_name
+  tunnel_secret = random_id.argo_secret.b64_std
+  config_src    = "local"
 
-  depends_on = [
+  depends_on    = [
     cloudflare_zero_trust_access_application.home_media_server
   ]
 }
@@ -138,28 +138,31 @@ resource "azurerm_key_vault_secret" "vpn_password" {
   key_vault_id = azurerm_key_vault.keyvault.id
 }
 
-resource "cloudflare_record" "home_media_server_cname" {
+resource "cloudflare_dns_record" "home_media_server_cname" {
   zone_id = data.azurerm_key_vault_secret.cloudflare_zone_id.value
   name    = var.cloudflare_application_name
-  content = cloudflare_zero_trust_tunnel_cloudflared.tunnel.cname
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.tunnel.id}.cfargotunnel.com"
   type    = "CNAME"
   proxied = true
+  ttl = 1
 }
 
-resource "cloudflare_record" "home_media_server_local_a" {
+resource "cloudflare_dns_record" "home_media_server_local_a" {
   zone_id = data.azurerm_key_vault_secret.cloudflare_zone_id.value
   name    = format("local.%s", var.cloudflare_application_name)
   content = var.local_network_ip_address
   type    = "A"
   proxied = false
+  ttl     = 1
 }
 
-resource "cloudflare_record" "home_assistant_cname" {
+resource "cloudflare_dns_record" "home_assistant_cname" {
   zone_id = data.azurerm_key_vault_secret.cloudflare_zone_id.value
   name    = var.home_assistant_subdomain
-  content = cloudflare_zero_trust_tunnel_cloudflared.tunnel.cname
+  content = "${cloudflare_zero_trust_tunnel_cloudflared.tunnel.id}.cfargotunnel.com"
   type    = "CNAME"
   proxied = true
+  ttl = 1
 }
 
 
@@ -174,7 +177,7 @@ resource "cloudflare_zero_trust_access_identity_provider" "azure_ad_oauth" {
   account_id = data.azurerm_key_vault_secret.cloudflare_account_id.value
   name       = "Azure Active Directory via Home Media Server App Registration"
   type       = "azureAD"
-  config {
+  config     = {
     client_id       = var.app_registration_client_id
     client_secret   = azurerm_key_vault_secret.client_secret.value
     directory_id    = data.azurerm_client_config.current.tenant_id
@@ -185,13 +188,12 @@ resource "cloudflare_zero_trust_access_identity_provider" "azure_ad_oauth" {
 resource "cloudflare_zero_trust_access_group" "home_media_server_users" {
   account_id = data.azurerm_key_vault_secret.cloudflare_account_id.value
   name       = "Home media server users"
-
-  include {
-    azure {
+  include = [{
+    azure_ad = {
       identity_provider_id = cloudflare_zero_trust_access_identity_provider.azure_ad_oauth.id
-      id                   = [var.entra_id_access_group_object_id]
+      id                   = var.entra_id_access_group_object_id
     }
-  }
+  }]
 }
 
 resource "cloudflare_zero_trust_access_policy" "allow_home_media_server_users_based_on_entra_id_group" {
@@ -199,9 +201,9 @@ resource "cloudflare_zero_trust_access_policy" "allow_home_media_server_users_ba
   name       = "Allow home media server users"
   decision   = "allow"
 
-  include {
-    group = [cloudflare_zero_trust_access_group.home_media_server_users.id]
-  }
+  include = [{
+    group = cloudflare_zero_trust_access_group.home_media_server_users.id
+  }]
 }
 
 resource "kubernetes_namespace" "home-media-server" {
