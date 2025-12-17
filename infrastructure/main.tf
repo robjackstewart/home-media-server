cf1ac3901aed4d4fd51ab6f4938c2dffb6735059
Rob Jack Stewart <robjackstewart@gmail.com>
2025-12-17 12:45:04 +0000

    use kubernetes_namespace_v1

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -198,7 +198,7 @@ resource "cloudflare_zero_trust_access_policy" "allow_home_media_server_users_ba
   }]
 }
 
-resource "kubernetes_namespace" "home-media-server" {
+resource "kubernetes_namespace_v1" "home-media-server" {
   metadata {
     name = var.kubernetes_namespace
   }
@@ -207,7 +207,7 @@ resource "kubernetes_namespace" "home-media-server" {
 resource "kubernetes_secret" "argo_tunnel_credentials" {
   metadata {
     name = var.cloudflare_tunnel_credential_secret_name
-    namespace = var.kubernetes_namespace
+    namespace = kubernetes_namespace_v1.home-media-server.metadata[0].name
   }
 
   data = {
@@ -218,7 +218,7 @@ resource "kubernetes_secret" "argo_tunnel_credentials" {
 resource "kubernetes_secret" "vpn_credentials" {
   metadata {
     name = var.transmission_vpn_secret_name
-    namespace = var.kubernetes_namespace
+    namespace = kubernetes_namespace_v1.home-media-server.metadata[0].name
   }
 
   data = {
